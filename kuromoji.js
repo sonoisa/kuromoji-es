781aa25b1da60cbe8594e1dcbe7a480b83d23212
Taisuke Fukuno <fukuno@jig.jp>
2022-10-07 03:39:14 +0900

    test change dicurl

--- kuromoji.js
+++ kuromoji.js
@@ -1,6 +1,7 @@
 import { kuromoji as _kuromoji } from "./build/kuromoji.js";
 
-const DIC_URL = "https://code4fukui.github.io/kuromoji-es/dict/";
+//const DIC_URL = "https://code4fukui.github.io/kuromoji-es/dict/";
+const DIC_URL = "../dict/";
 
 export const kuromoji = {
   createTokenizer: async () => {
