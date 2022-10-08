fbc980eecbc0f07bd23a45b1de3c23e3e61f783f
Taisuke Fukuno <fukuno@jig.jp>
2022-10-08 21:20:54 +0900

    change demo

--- example/load-deno2.js
+++ example/load-deno2.js
@@ -18,5 +18,5 @@
 import { kuromoji } from "../kuromoji.js";
 
 const tokenizer = await kuromoji.createTokenizer();
-const path = tokenizer.tokenize("私は家にいる");
+const path = tokenizer.tokenize("すもももももももものうち");
 console.log(path);
